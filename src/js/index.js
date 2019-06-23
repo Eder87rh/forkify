cd7077978099ddcca57fe1541403311069543c26
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-06-23 17:17:31 -0500

    Making our first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,17 @@
-// Global app controller
+// https://www.food2fork.com/api/search
+// 6dfafa8e00e40f71b4368d9dd62f9cee
 
-import num from './test';
+import axios from 'axios';
 
-const x = 23;
+async function getResults(query) {
+  const key = "6dfafa8e00e40f71b4368d9dd62f9cee";
+  try {
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`)
+    const recipes = res.data.recipes;
+    console.log(res);
+  } catch (error) {
+    alert(error)
+  }
+}
 
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`);
+getResults('pizza');
