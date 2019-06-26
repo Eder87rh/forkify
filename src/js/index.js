f737e8277f702e78ef87dc3b32c60e173aa711c4
Eder Ramirez <eder.ramirez87@gmail.com>
2019-06-26 18:05:13 -0500

    building the recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -28,12 +28,17 @@ const controlSearch = async () => {
     searchView.clearResults();
     renderLoader(elements.searchRes);
 
-    // 4. Search for recipes
-    await state.search.getResults();
+    try {
+      // 4. Search for recipes
+      await state.search.getResults();
+  
+      // 5. Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch(error) {
+      alert("Something wrong with the search...")
+    }
 
-    // 5. Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
   }
 }
 
@@ -45,18 +50,45 @@ elements.searchForm.addEventListener('submit', (e) => {
 elements.searchResPages.addEventListener('click', e => {
   const btn = e.target.closest('.btn-inline');
   console.log(btn);
+
   if (btn) {
     const goToPage = parseInt(btn.dataset.goto, 10);
     searchView.clearResults();
     searchView.renderResults(state.search.result, goToPage);
     console.log(goToPage);
   }
-
 });
 
 /**
  * Recipe controller
  */
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace("#", "");
+  console.log(id);
+
+  if (id) {
+    // Prepare UI fo changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+  
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+  
+      // Render recipe
+      console.log(state.recipe);
+    } catch (error) {
+      alert("Error processing recipe!");
+    }
+  }
+};
+
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
