d8337992573d18cbd784c39569c5469deee950c5
Eder Ramírez Hernández <eder.ramirez87@gmail.com>
2019-06-30 15:56:51 -0500

    Persisting data with localstorage

--- src/js/views/likesView.js
+++ src/js/views/likesView.js
@@ -2,7 +2,7 @@ import { elements } from './base';
 import { limitRecipeTitle } from './searchView';
 
 export const toggleLikeBtn = isLiked => {
-  const iconString = isLiked ? 'icon-heart' : 'icon-heart-outline';
+  const iconString = isLiked ? 'icon-heart' : 'icon-heart-outlined';
   document.querySelector('.recipe__love use').setAttribute('href', `img/icons.svg#${iconString}`)
 };
 
